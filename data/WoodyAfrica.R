44e5bd6cd12bdbcd5da6e64d221810b48c1c91f6
ruan-de-wet <rfdewet@gmail.com>
2019-12-30 11:21:17 +0200

    Initial commit

---
+++ data/WoodyAfrica.R
@@ -0,0 +1,24 @@
+#' Woody cover in Africa
+#'
+#' Woody cover percentage in Africa relative to mean annual precipitation (mm), 
+#' as published by Sankaran et al. 2005 in Nature.
+#'
+#' @docType data
+#' 
+#' @keywords datasets
+#' 
+#' @format A data frame with 854 rows and 2 variables:
+#' \describe{
+#'   \item{MAP}{Mean annual precipitation, in mm}
+#'   \item{Cover}{Woody cover, in %}
+#' }
+#' 
+#' @references Sankaran et al. (2005) Determinants of woody cover in African savannas. Nature 438, p846–849.
+#' 
+#' @source \url{https://doi.org/10.1038/nature04070}
+#' 
+#' @examples 
+#' data(WoodyAfrica)
+#' \donttest[plot(WoodyAfrica$MAP, WoodyAfrica$Cover)]
+#'
+"WoodyAfrica"

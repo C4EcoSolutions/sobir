26f70a0129214a869c5b5c7c4fec3d2c06c57248
ruan-de-wet <rfdewet@gmail.com>
2019-12-31 11:52:16 +0200

    Expanded description

--- data/WoodyTowoomba.R
+++ data/WoodyTowoomba.R
@@ -12,7 +12,7 @@
 #' \describe{
 #'   \item{Treatment}{12 treatments of a combination of superphosphate (SP0-SP2; 0, 233, 466 kg/ha/yr) and ammonium sulphate (AS0-AS3; 0, 146, 291, 583, 1166 kg/ha/yr) fertilisation applications.}
 #'   \item{TreeNum}{Number of trees present following three decades of fertilisation and three subsequent decades of passive protection.}
-#'   \item{EC, pH, B, C, Na, Mg, P, S, N, K, Ca, Mn, Cu, Zn, N_NO3, N_NH4}{Soil nutrient and element concentration in mg/kg, except for pH (unitless), EC (uS/cm) and C (%)}
+#'   \item{EC, pH, B, C, Na, Mg, P, S, N, K, Ca, Mn, Cu, Zn, N_NO3, N_NH4}{Soil nutrient and element concentration of the pedoderm (0-2 cm) in mg/kg, except for pH (unitless), EC (uS/cm) and C (%).}
 #' }
 #' 
 #' @references Mills et al. (2017) Effects of anabolic and catabolic nutrients on woody plant encroachment after long-term experimental fertilization in a South African savanna. PLoS ONE 12(6), p1-24.

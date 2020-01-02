b401ac3e2826c68afadf0df93b2a1fb6d7bf4815
ruan-de-wet <rfdewet@gmail.com>
2020-01-02 09:27:17 +0200

    Conforming to CRAN check requirements

--- R/WoodyAfrica.R
+++ R/WoodyAfrica.R
@@ -2,16 +2,15 @@
 #'
 #' Woody cover percentage in Africa relative to mean annual precipitation (mm), 
 #' as published by Sankaran et al. 2005 in Nature.
+#' 
+#' MAP = Mean annual precipitation, in mm
+#' Cover = Woody cover, in %
 #'
 #' @docType data
 #' 
 #' @keywords datasets
 #' 
-#' @format A data frame with 854 rows and 2 variables:
-#' \describe{
-#'   \item{MAP}{Mean annual precipitation, in mm}
-#'   \item{Cover}{Woody cover, in %}
-#' }
+#' @format A data frame with 854 rows and 2 variables
 #' 
 #' @references Sankaran et al. (2005) Determinants of woody cover in African savannas. Nature 438, p846–849.
 #' 
--- R/WoodyTowoomba.R
+++ R/WoodyTowoomba.R
@@ -4,16 +4,15 @@
 #' four soil fertilisation treatments and 16 soil nutrient and element concentrations, 
 #' as published by Mills et al. 2017 in PLoS ONE.
 #'
+#' Treatment = 12 treatments of a combination of superphosphate (SP0-SP2; 0, 233, 466 kg/ha/yr) and ammonium sulphate (AS0-AS3; 0, 146, 291, 583, 1166 kg/ha/yr) fertilisation applications.
+#' TreeNum = Number of trees present following three decades of fertilisation and three subsequent decades of passive protection.
+#' EC, pH, B, C, Na, Mg, P, S, N, K, Ca, Mn, Cu, Zn, N_NO3, N_NH4 = Soil nutrient and element concentration of the pedoderm (0-2 cm) in mg/kg, except for pH (unitless), EC (uS/cm) and C (%).
+#'
 #' @docType data
 #' 
 #' @keywords datasets
 #' 
-#' @format A data frame with 60 rows and 18 variables:
-#' \describe{
-#'   \item{Treatment}{12 treatments of a combination of superphosphate (SP0-SP2; 0, 233, 466 kg/ha/yr) and ammonium sulphate (AS0-AS3; 0, 146, 291, 583, 1166 kg/ha/yr) fertilisation applications.}
-#'   \item{TreeNum}{Number of trees present following three decades of fertilisation and three subsequent decades of passive protection.}
-#'   \item{EC, pH, B, C, Na, Mg, P, S, N, K, Ca, Mn, Cu, Zn, N_NO3, N_NH4}{Soil nutrient and element concentration of the pedoderm (0-2 cm) in mg/kg, except for pH (unitless), EC (uS/cm) and C (%).}
-#' }
+#' @format A data frame with 60 rows and 18 variables
 #' 
 #' @references Mills et al. (2017) Effects of anabolic and catabolic nutrients on woody plant encroachment after long-term experimental fertilization in a South African savanna. PLoS ONE 12(6), p1-24.
 #' 
